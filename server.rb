cedeba526e420dda849ef49961404bc54030219b
Peter Ohler <peter@ohler.com>
2018-06-04 17:08:06 -0400

    Fixed log configuration

--- server.rb
+++ server.rb
@@ -1,29 +1,27 @@
 require 'agoo'
 
-Agoo::Server.init(3000, 'public',
-  thread_count: 0,
-  worker_count: 1,
-  root_first: true,
+Agoo::Log.configure(dir: 'log',
+		    console: true,
+		    classic: true,
+		    colorize: true,
+		    states: {
+		      INFO: true,
+		      DEBUG: false,
+		      connect: true,
+		      request: true,
+		      response: true,
+		      eval: true,
+		      push: true,
+		    })
 
-  log_dir: 'tmp/log',
-  log_classic: true,
-  log_colorize: true,
-  log_console: true,
-  log_states: {
-    INFO: true,
-    DEBUG: true,
-    connect: true,
-    request: true,
-    response: true,
-    eval: true
-  })
+Agoo::Server.init(3000, 'public',
+		  thread_count: 0,
+		  worker_count: 1,
+		  root_first: true,
+		 )
 
 class HelloWorld
   def call(env)
     [200, {}, ["Found"]]
   end
 end
-
-Agoo::Server.handle(nil, '**', HelloWorld.new)
-Agoo::Server.start
-
